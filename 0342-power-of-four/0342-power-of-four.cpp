bac3041bfec11b8d028e7c927e833462f8c4388a
Hemant4568 <86296518+Hemant4568@users.noreply.github.com>
2023-03-15 00:31:06 +0530

    Time: 3 ms (50.93%), Space: 6.2 MB (5.26%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+       
+        if(n==0)
+            return 0;
+        float x=log(n)/log(4);
+        if(ceil(x)==floor(x))
+        return true;
+        else 
+            return false;
+    }
+};
