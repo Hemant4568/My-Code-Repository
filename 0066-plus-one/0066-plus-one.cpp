620eb905f41309733d772aaac82c167b5c0128c0
Hemant4568 <86296518+Hemant4568@users.noreply.github.com>
2023-03-17 00:22:26 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (89.25%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+       /* vector<int>ans;
+        int n=digits.size();
+        long long int sum=0;
+          for(int i=0;i<digits.size();i++)
+          {
+              if(n<0)
+                  break;
+              sum=sum+(pow(10,(n-1))*digits[i]);
+              n--;
+         }
+        sum+=1;
+      while(sum>0)
+      {
+         
+          int a=sum%10;
+          ans.push_back(a);
+          sum=sum/10;
+      }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+    
+};*/
+
+int n=digits.size()-1;
+        while(n>=0){
+            if(digits[n]==9)digits[n]=0;
+            else{
+                digits[n]+=1;
+                return digits;
+            }
+            n--;
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
