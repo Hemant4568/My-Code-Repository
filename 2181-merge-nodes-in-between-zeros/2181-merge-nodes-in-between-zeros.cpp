87a48bb2c8cc3ce9acabbe11f4da6415c1e90657
Hemant4568 <86296518+Hemant4568@users.noreply.github.com>
2023-04-14 02:44:43 +0530

    Time: 737 ms (69.82%), Space: 253.1 MB (75.31%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -33,21 +33,4 @@ public:
         return head;
     }
 };
-   /*ListNode*p = head,*q = head;
-        ListNode* ans = q;
-        int x = 0;
-        while(p!= NULL){
-            x = 0;
-            p = p->next;
-            if(!p)break;
-            while(p->val!=0){
-                x += p->val;
-                p = p->next;
-            }
-            // p = p->next;
-            q->val = x;
-            ans = q;
-            q = q->next;
-        }
-        ans->next = NULL;
-        return head;*/
+   
