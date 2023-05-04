d1273d0531a1a597547cc2e7cc614c343e6344ed
Hemant4568 <86296518+Hemant4568@users.noreply.github.com>
2023-05-04 22:28:02 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
@@ -9,23 +9,25 @@ public:
     vector<int> findTwoElement(vector<int> arr, int n) {
         // code here
          sort(arr.begin(),arr.end());
+         long long int sum;
     vector<int>ans;
+    if(n%2==0)
+     sum=((n/2)*(n+1));
+    else
+     sum=n*((n+1)/2);
+
+    long long int sum1=0;
+    for(int i=0;i<n;i++)
+    {
+        sum1+=arr[i];
+    }
     for(int i=0;i<n-1;i++)
     {
         if(arr[i]==arr[i+1])
         {
             ans.push_back(arr[i]);
-            arr.erase(arr.begin()+i);
+            ans.push_back(sum-sum1+arr[i]);
             break;
-        }
-        
-    }
-    
-    for(int j=0;j<n;j++)
-    {
-        if(arr[j]!=j+1){
-        ans.push_back(j+1);
-        break;
         }
     }
     return ans;
