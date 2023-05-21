408678c67ac71b510402d21998682a3e877701b8
Hemant4568 <86296518+Hemant4568@users.noreply.github.com>
2023-05-22 01:19:02 +0530

    Time: 3 ms (52.35%), Space: 8.6 MB (83.59%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -27,7 +27,8 @@ public:
 
 int n=digits.size()-1;
         while(n>=0){
-            if(digits[n]==9)digits[n]=0;
+            if(digits[n]==9)
+                digits[n]=0;
             else{
                 digits[n]+=1;
                 return digits;
