2657214264f094ac4be1993da116eb7d7ea03c1f
Hemant4568 <86296518+Hemant4568@users.noreply.github.com>
2023-09-03 11:50:25 +0530

    Time: 99 ms (70.28%), Space: 64.7 MB (41.39%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -15,11 +15,11 @@ public:
     int rangeSumBST(TreeNode* root, int low, int high) {
         if(root==NULL)
             return NULL;
-                rangeSumBST(root->left,low,high);
 
         if(root->val<=high && root->val>=low)
             sum+=root->val;
-        
+                        rangeSumBST(root->left,low,high);
+
             rangeSumBST(root->right,low,high);
         
         return sum;
