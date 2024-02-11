e2f267554631d6b72d0ee0b86ce6e4b8811aa8c4
Hemant4568 <86296518+Hemant4568@users.noreply.github.com>
2024-02-11 19:06:00 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (7.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,11 +5,10 @@ public:
             return 0;
         if(n==1)
             return 1;
-        if(n==2)
-            return 2;
+        
        vector<int>dp(n+1,0);
-        dp[2]=2;
         dp[1]=1;
+        dp[2]=2;
         for(int i=3;i<=n;i++)
         {
             dp[i]=dp[i-1]+dp[i-2];
