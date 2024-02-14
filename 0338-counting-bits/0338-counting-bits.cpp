64ca5f36574e89632343b40aeeb89cf9c8483066
Hemant4568 <86296518+Hemant4568@users.noreply.github.com>
2024-02-14 17:35:42 +0530

    Time: 5 ms (52.43%), Space: 9 MB (42.90%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>ans;
+        vector<int>ans(n+1,0);
         for(int i=0;i<=n;i++)
         {
-            int sum=0;
-            int num=i;
-            while(num)
-            {
-                sum+=num%2;
-                num/=2;
-            }
-            ans.push_back(sum);
+            ans[i]=ans[i>>1]+(i&1);
         }
         return ans;
     }
