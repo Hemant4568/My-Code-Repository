8ec0372e1955c8bea4312c9d29d4cafeae2d82c6
Hemant4568 <86296518+Hemant4568@users.noreply.github.com>
2024-03-10 00:18:50 +0530

    Time: 4 ms (44.06%), Space: 7.7 MB (16.59%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int c=0;
+        for(int i=s.size()-1;i>=0;i--)
+        {
+            if(s[i]==' ' && c==0)
+                continue;
+            else
+            {
+               if(s[i]!=' ')
+                   c++;
+                else
+                    break;
+            }
+        }
+        return c;
+    }
+};
