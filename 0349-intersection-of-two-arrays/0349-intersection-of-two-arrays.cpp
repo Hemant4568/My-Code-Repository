5bcf4373508c6c2b07b10d4289fc55602b0e8a1f
Hemant4568 <86296518+Hemant4568@users.noreply.github.com>
2024-03-10 14:29:12 +0530

    Time: 4 ms (68.61%), Space: 12.6 MB (51.34%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -7,10 +7,9 @@ public:
         for(int i=0;i<nums1.size();i++)
             mp[nums1[i]]++;
         for(int i=0;i<nums2.size();i++)
-            if(mp.find(nums2[i])!=mp.end())
-                s.insert(nums2[i]);
-        for(auto it:s)
-            ans.push_back(it);
+            if(mp.find(nums2[i])!=mp.end()){
+            ans.push_back(nums2[i]);
+        mp.erase(nums2[i]);}
         return ans;
     }
 };
