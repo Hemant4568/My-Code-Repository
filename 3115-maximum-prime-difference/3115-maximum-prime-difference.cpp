62a15b6e7f28ab7ca415e59e74b4b06dc3f86b57
Hemant4568 <86296518+Hemant4568@users.noreply.github.com>
2024-04-25 23:06:49 +0530

    Time: 111 ms (22.80%), Space: 110 MB (27.57%) - LeetHub

--- 3115-maximum-prime-difference/3115-maximum-prime-difference.cpp
+++ 3115-maximum-prime-difference/3115-maximum-prime-difference.cpp
@@ -3,31 +3,36 @@ public:
     bool isprime(int n)
     {
         if(n==1)
-            return false;
+            return 0;
         for(int i=2;i<n;i++)
         {
             if(n%i==0)
-                return false;
+                return 0;
         }
-        return true;
+        return 1;
     }
     int maximumPrimeDifference(vector<int>& nums) {
         vector<int>temp;
         for(int i=0;i<nums.size();i++)
         {
             if(isprime(nums[i]))
+            {
                 temp.push_back(i);
+            }
         }
-        if (temp.size() < 2) {
-        return 0;
-        }
-       int minVal = temp[0];
+            if (temp.size() < 2) {
+        return 0; 
+            }
+
+         int minVal = temp[0];
     int maxDiff = temp[1] - temp[0];
 
-    for (int i = 1; i <temp.size(); ++i) {
+    for (int i = 1; i < temp.size(); ++i) {
         maxDiff = max(maxDiff, temp[i] - minVal);
         minVal = min(minVal, temp[i]);
     }
-        return maxDiff;
+
+
+    return maxDiff;
     }
 };
