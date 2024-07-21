594f90f228affad342ebd39641640352c9ac20ff
Hemant4568 <86296518+Hemant4568@users.noreply.github.com>
2024-07-22 00:29:16 +0530

    Time: 3 ms (33.33%), Space: 8.5 MB (66.67%) - LeetHub

--- 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.cpp
+++ 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.cpp
@@ -8,8 +8,10 @@ public:
     return binary;
     }
     int minChanges(int n, int k) {
-        string temp=intToBinary(n);
-        string temp1=intToBinary(k);
+         bitset<32> binary1(n);
+    bitset<32> binary2(k);
+        string temp=binary1.to_string();
+        string temp1=binary2.to_string();
         cout<<temp1;
         if(temp==temp1)
             return 0;
