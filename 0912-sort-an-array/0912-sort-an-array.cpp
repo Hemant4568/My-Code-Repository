8a3403188b8182db0e758d11e5b2853f8672335d
Hemant4568 <86296518+Hemant4568@users.noreply.github.com>
2024-07-25 23:29:52 +0530

    Time: 201 ms (52.05%), Space: 101.1 MB (44.51%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        map<int,int>mp;
+        for(int i:nums)
+            mp[i]++;
+        vector<int>ans;
+        for(auto it:mp)
+        {
+            int i=it.second;
+            while(i!=0)
+            {
+                ans.push_back(it.first);
+                i--;
+            }
+        }
+        return ans;
+    }
+};
