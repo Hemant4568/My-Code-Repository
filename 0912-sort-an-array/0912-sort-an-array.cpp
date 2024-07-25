11c98c5fb8f63cc244f1a644bf51aee4b8901f16
Hemant4568 <86296518+Hemant4568@users.noreply.github.com>
2024-07-25 23:33:40 +0530

    Time: 88 ms (86.64%), Space: 70.2 MB (76.03%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        map<int,int>mp;
-        for(int i:nums)
-            mp[i]++;
-        vector<int>ans;
-        for(auto it:mp)
-        {
-            int i=it.second;
-            while(i!=0)
-            {
-                ans.push_back(it.first);
-                i--;
-            }
-        }
-        return ans;
+        // map<int,int>mp;
+        // for(int i:nums)
+        //     mp[i]++;
+        // vector<int>ans;
+        // for(auto it:mp)
+        // {
+        //     int i=it.second;
+        //     while(i!=0)
+        //     {
+        //         ans.push_back(it.first);
+        //         i--;
+        //     }
+        // }
+        // return ans;
+        sort(nums.begin(),nums.end());
+        return nums;
     }
 };
